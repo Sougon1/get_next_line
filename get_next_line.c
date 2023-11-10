1ff7c16e1564e457df32f114e20677709d077a63
Sougon1 <git@github.com:Sougon1/get_next_line.git>
2023-11-10 17:29:05 +0100

    fichier

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,53 @@
 /*   By: ghumm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:06:16 by ghumm             #+#    #+#             */
-/*   Updated: 2023/11/10 13:49:58 by ghumm            ###   ########.fr       */
+/*   Updated: 2023/11/10 17:22:53 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+/*
+static ssize_t	line()
+{
+	
+}
+
+static char	*remainder()
+{
+	
+}
+*/
 
 char	*get_next_line(int fd)
 {
-	
+	char		buffer[BUFFER_SIZE + 1];
+	static char	*remainder;
+	char		*line;
+	ssize_t		read_b;
+	char		*newline;
+
+	remainder = NULL;
+	if (fd < 0)
+		return (NULL);
+	if (remainder != NULL || *remainder != '\0')
+	{
+		line = remainder;
+		remainder = NULL;
+		return (line);
+	}
+	read_b = read(fd, buffer, BUFFER_SIZE + 1);
+	if (read_b <= 0)
+		return (NULL);
+	buffer[read_b] = '\0';
+	line = buffer;
+	newline = ft_strchr(buffer, '\n');
+	if (newline != NULL)
+	{
+		*newline = '\0';
+		remainder = newline + 1;
+	}
+	else
+	{
+		remainder= buffer + read_b;
+	}
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,20 @@
 /*   By: ghumm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 13:32:27 by ghumm             #+#    #+#             */
-/*   Updated: 2023/11/10 14:38:14 by ghumm            ###   ########.fr       */
+/*   Updated: 2023/11/10 17:28:56 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE = 256
+# define BUFFER_SIZE 256
 
 # include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
+char    *ft_strchr(const char *s, int c);
 
 
 
