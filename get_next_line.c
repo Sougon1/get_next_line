4523e6c2b5d670a24ce17b3752aeb8a4c8227b40
Guillaume Humm <ghumm@k1r4p2.42mulhouse.fr>
2023-11-21 16:50:08 +0100

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -9,8 +9,14 @@
 /*   Updated: 2023/11/20 11:38:50 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "get_next_line.h"
+//#include "get_next_line.h"
 
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 /*
 stati char	line()
 {
@@ -113,9 +119,10 @@ char *ft_strchr(const char *s, int c)
     return (NULL);
 }
 
+/*
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
+	static char	buffer[BUFFER_SIZE + 1];
 	static char	*remainder;
 	char		*line;
 	int			read_b;
@@ -123,31 +130,37 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!remainder)
-		remainder = ft_strdup("");
 	read_b = read(fd, buffer, BUFFER_SIZE);
 	while (read_b > 0 && !ft_strchr(buffer, '\n'))
 	{
 		buffer[read_b] = '\0';
-		temp = remainder;
-		remainder = ft_strjoin(temp, buffer);
-/*		if (temp)
+		temp = remainder;		
+		if (temp)
 			remainder = ft_strjoin(temp, buffer);
 		else
-			remainder = ft_strdup(buffer);*/
+			remainder = ft_strdup(buffer);
+		if (!remainder)
+		{
+			free(temp);
+			return (NULL);
+		}
 		free(temp);
 		read_b = read(fd, buffer, BUFFER_SIZE);
 	}
-	if (read_b <= 0 && (!remainder || remainder [0] == '\0'))
+	if (read_b <= 0 || (!ft_strchr(buffer, '\n') && (!remainder || remainder [0] == '\0')))
 	{
 		free(remainder);
 		return (NULL);
 	}
 	int len = 0;
-	if (remainder[len] != '\n' && remainder[len] != '\0')
+	while (remainder[len] != '\n' && remainder[len] != '\0')
 		len++;
 	line = ft_substr(remainder, 0, len);
-	temp = remainder;
+	if (!line)
+	{
+		free(remainder);
+		return (NULL);
+	}
 	if (remainder[len] == '\n')
 	{
 		temp = remainder;
@@ -159,9 +172,8 @@ char	*get_next_line(int fd)
 		free(remainder);
 		remainder = NULL;
 	}
-//	free(temp);
 	return (line);
-}
+}*/
 
 
 /*
@@ -201,5 +213,125 @@ char	*get_next_line(int fd)
 	free(temp);
 
 	return (line);
+}*/
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 42
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE];
+	static char	*remainder = NULL;
+	char		*line;
+	int			read_b;
+	char		*temp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+
+	read_b = read(fd, buffer, BUFFER_SIZE);
+
+	while (read_b > 0)
+	{
+		buffer[read_b] = '\0';
+		temp = remainder;
+		remainder = (temp) ? ft_strjoin(temp, buffer) : ft_strdup(buffer);
+		free(temp);
+
+		if (!remainder)
+			return (NULL);
+
+		if (ft_strchr(remainder, '\n'))
+			break;
+
+		read_b = read(fd, buffer, BUFFER_SIZE);
+	}
+
+	if (read_b <= 0 && (!remainder || remainder[0] == '\0'))
+	{
+		free(remainder);
+		return (NULL);
+	}
+
+	int len = 0;
+	while (/*remainder[len] != '\n' &&*/ remainder[len] != '\0')
+		len++;
+	
+	
+	line = ft_substr(remainder, 0, len);
+
+	if (!line)
+	{
+		free(remainder);
+		return (NULL);
+	}
+	
+	if (remainder[len] == '\n')
+	{
+		temp = remainder;
+		remainder = ft_strdup(temp + len + 1);
+		free(temp);
+	}
+	else
+	{
+		free(remainder);
+		remainder = NULL;
+	}
+
+
+	return (line);
 }
-*/
+
+
+
+void print_test_result(int test_number, const char *result, const char *expected)
+{
+    printf("Test %d: ", test_number);
+    
+    if (result == NULL || expected == NULL)
+    {
+        printf("FAILED (Result or Expected is NULL)\n\n");
+    }
+    else if (strcmp(result, expected) == 0)
+    {
+        printf("PASSED\n\n");
+    }
+    else
+    {
+        printf("FAILED \nResult: \"%s\" \nExpected: \"%s\"\n\n", result, expected);
+    }
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	// Test 1: Fichier avec une seule ligne
+	fd = open("text1.txt", O_RDONLY);
+	line = get_next_line(fd);
+	print_test_result(1, line, "");
+	free(line);
+	close(fd);
+
+	// Test 2: Fichier vide
+	fd = open("text2.txt", O_RDONLY);
+	line = get_next_line(fd);
+	print_test_result(2, line, "a\nb\n\nc");
+	free(line);
+	close(fd);
+
+	// Test 3: Fichier sans caractère de nouvelle ligne
+	fd = open("text3.txt", O_RDONLY);
+	line = get_next_line(fd);
+	print_test_result(3, line, "\n\n\n\n\n\n\n\n\n");
+	free(line);
+	close(fd);
+	
+	// ... Ajoutez d'autres tests ...
+
+	return (0);
+}
+
